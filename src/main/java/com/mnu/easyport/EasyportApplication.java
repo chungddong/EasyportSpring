f8ec29f61b7178a963f9c2a261e6846719c39c79
CHUNGMAN <chungman010918@gmail.com>
2024-04-24 13:48:00 +0900

    first commit

---
+++ src/main/java/com/mnu/easyport/EasyportApplication.java
@@ -0,0 +1,15 @@
+package com.mnu.easyport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyportApplication.class, args);
+
+		
+	}
+
+}
