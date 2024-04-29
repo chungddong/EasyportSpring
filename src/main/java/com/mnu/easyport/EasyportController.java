d1b3bf9dae36b8c5085b91055cbba86a9268701f
CHUNGMAN <chungman010918@gmail.com>
2024-04-29 18:52:13 +0900

    중간백업

--- src/main/java/com/mnu/easyport/EasyportController.java
+++ src/main/java/com/mnu/easyport/EasyportController.java
@@ -1,17 +1,26 @@
 package com.mnu.easyport;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class EasyportController {
 
     @Autowired
     private SiteUserRepository userRepository;
+
 
     @GetMapping(path = "/")
     public String mains(Model model) {
@@ -27,5 +36,7 @@ public class EasyportController {
         model.addAttribute("name", user.getName());
         return "signup_done";
     }
+
+    
 
 }
