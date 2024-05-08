2deb98cf52b7b3e2b5c0c3a1bd1dec107e2f406d
CHUNGMAN <chungman010918@gmail.com>
2024-05-08 16:03:29 +0900

    thymeleaft template 오류 관련 restcontroller로 교체 테스트

--- src/main/java/com/mnu/easyport/EasyportController.java
+++ src/main/java/com/mnu/easyport/EasyportController.java
@@ -18,13 +18,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.persistence.Entity;
 import jakarta.servlet.http.HttpSession;
 
-@Controller
+@RestController
 public class EasyportController {
 
     @Autowired
