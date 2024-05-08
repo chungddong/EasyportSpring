e7915c7a87d889860b4482e008a65fbecf1d09a3
CHUNGMAN <chungman010918@gmail.com>
2024-05-08 17:38:26 +0900

    prefix 설정

--- src/main/java/com/mnu/easyport/EasyportController.java
+++ src/main/java/com/mnu/easyport/EasyportController.java
@@ -18,16 +18,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.persistence.Entity;
 import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequiredArgsConstructor
+@Controller
 public class EasyportController {
 
     @Autowired
